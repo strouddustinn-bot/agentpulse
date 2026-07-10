06d5251fe82cd86c7a4dc5e8bd5cb6ebc2612730
Dustinn Stroud <strouddustinn@gmail.com>
2026-07-09 22:57:34 -0400

    Add Cloudflare provider to Terraform configuration

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -16,6 +16,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 }
 
