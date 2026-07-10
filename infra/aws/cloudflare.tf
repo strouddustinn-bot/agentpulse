f3358ffda94c268d9fae6dbf45244247623a651b
Dustinn Stroud <strouddustinn@gmail.com>
2026-07-09 22:58:36 -0400

    Simplify Cloudflare provider configuration
    Removed unnecessary terraform block for Cloudflare provider.

--- infra/aws/cloudflare.tf
+++ infra/aws/cloudflare.tf
@@ -2,15 +2,6 @@
 # Cloudflare DNS integration – manages api.agentpulse.ca CNAME -> ALB
 ################################################################################
 
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
